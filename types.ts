6d01691c78d11dad346d674475542593be6abe77
summ791 <rithanyagopinathan2005@gmail.com>
2025-11-20 22:45:07 +0530

    Update types.ts

--- types.ts
+++ types.ts
@@ -3,13 +3,14 @@ import React from 'react';
 export type Screen = 'welcome' | 'home' | 'book' | 'profile';
 
 export interface NavItem {
-  id: Screen;
-  label: string;
-  icon: React.ComponentType<any>;
+  id: Screen;
+  label: string;
+  icon: React.ComponentType<any>;
 }
 
 export interface ProfileData {
-  email: string;
-  phone: string;
-  imageUrl: string;
+  email: string;
+  phone: string;
+  imageUrl: string;
+  password?: string;
 }
