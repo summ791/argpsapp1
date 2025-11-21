fbbd2056d0be66c561c9009705f438b766faf0ee
summ791 <rithanyagopinathan2005@gmail.com>
2025-11-21 08:03:05 +0530

    Add newline at end of supabaseClient.ts

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
+// Your Supabase Credentials
 const SUPABASE_URL = 'https://bnqsnqumzywxttzojvtk.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJucXNucXVtenl3eHR0em9qdnRrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM2NTE5MTMsImV4cCI6MjA3OTIyNzkxM30.2cYb8lm5U20H5pi23xrEBYGsEeEAY-hM-Rz9HX65TK0';
 
